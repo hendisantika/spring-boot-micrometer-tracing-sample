b91169737e3d9f153b3b2cf9ecee162da5f2b16e
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 10:23:09 +0700

    Add customerHttpClient 🫸🌀✏️📗🐧🐳

--- client/src/main/java/id/my/hendisantika/client/ClientApplication.java
+++ client/src/main/java/id/my/hendisantika/client/ClientApplication.java
@@ -2,6 +2,10 @@ package id.my.hendisantika.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -10,4 +14,15 @@ public class ClientApplication {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
+	@Bean
+	CustomerHttpClient customerHttpClient(WebClient.Builder builder) {
+		return HttpServiceProxyFactory
+				.builder()
+				.clientAdapter(WebClientAdapter.forClient(builder
+						.baseUrl("http://localhost:9090")
+						.build()
+				))
+				.build()
+				.createClient(CustomerHttpClient.class);
+	}
 }
