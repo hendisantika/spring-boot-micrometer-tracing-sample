dd74af99b67b2301de5d236d9f14096c8a9ca53d
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 10:21:09 +0700

    Add Client Module 🫸🌀✏️📗🐧🐳

---
+++ client/src/main/java/id/my/hendisantika/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
